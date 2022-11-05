e343f906444fa2327de66ef0e65f14b132ec41f8
Laércio <laercioamedeiros@gmail.com>
2022-11-05 14:42:56 -0300

    feat:ModalEditProfile/ModalCreateRequest/ModalEditRequest

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,18 +5,24 @@ import ModalLogin from "./componentsHome/ModalLogin";
 import ModalRegister from "./componentsHome/ModalRegister";
 import ModalRegisterOng from "./componentsHome/ModalRegisterOng";
 import ModalRegisterDev from "./componentsHome/ModalRegisterDev";
+import ModalEditProfile from "../DashboardDev/ModalEditProfile";
+import ModalCreateResquest from "../DashboardOng/ModalCreateResquest";
+import ModalEditRequest from "../DashboardOng/ModalEditRequest";
+
 
 const HomePage = () => {
   const { actualSectionHome } = useUserContext();
 
   return (
     <>
-      {actualSectionHome == "login" && <ModalLogin />}
+    
+     <ModalEditRequest />
+      {/* {actualSectionHome == "login" && <ModalLogin />}
       {actualSectionHome == "register" && <ModalRegister />}
       {actualSectionHome == "registerDev" && <ModalRegisterDev />}
       {actualSectionHome == "registerOng" && <ModalRegisterOng />}
       <Header />
-      <Outlet />
+      <Outlet /> */}
     </>
   );
 };
